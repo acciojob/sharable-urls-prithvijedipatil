fb9718dfed7f3df62ab6d94328e9aea69307814c
prithvijedipatil <prithvipatil09@gmail.com>
2023-09-11 12:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+ document.getElementById('myForm').addEventListener('submit', function (e) {
+            e.preventDefault(); // Prevent form submission
+
+            // Get values from input fields
+            const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+
+            // Construct the query string
+            const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+
+            // Update the h3 element with the new URL
+            const urlElement = document.getElementById('url');
+            urlElement.textContent = `https://localhost:8080/${queryString}`;
+        });
